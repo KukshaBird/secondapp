874932baaaf028222bfc569a7ea51e03878ef5d8
KukshaBird <samoilovartem1989@gmail.com>
2025-04-21 22:49:05 +0300

    Handle collections in create word process.

--- src/screens/CreateWordScreen.tsx
+++ src/screens/CreateWordScreen.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     View,
     Text,
     StatusBar,
     SafeAreaView,
 } from 'react-native';
-import { CreateWordDto, Word } from "../types/word.types.ts";
+import { Collection, CreateWordDto, Word } from "../types/word.types.ts";
 import { COLORS } from "../constants";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { WordsRepository } from "../repository/words.repository.ts";
@@ -15,6 +15,8 @@ import { WordForm } from "../components/AdminPage";
 import ImagePersistorService from "../services/ImagePersistor.service.ts";
 import { useConnectDatabase } from "../hooks/useConnectDatabase.tsx";
 import { AdminStackParamList } from "./AdminPanelScreen.tsx";
+import { CollectionsRepository } from "../repository/collections.repository.ts";
+import CollectionsService from "../services/collections.service.ts";
 
 type CreateWordScreenNavigationProp = NativeStackNavigationProp<AdminStackParamList, 'WordForm'>;
 
@@ -25,6 +27,19 @@ interface WordFormProps {
 export const WordFormScreen = ({ navigation }: WordFormProps) => {
     const persistor = new ImagePersistorService();
     const { db, isConnecting } = useConnectDatabase();
+    const [collections, setCollections] = useState<Collection[]>([])
+
+    useEffect(() => {
+        if (db) {
+            const collectionsRepository = new CollectionsRepository(db);
+            const collectionsService = new CollectionsService(collectionsRepository);
+            const getAllCollections = async () => {
+                const items = await collectionsService.getCollections();
+                setCollections(items);
+            }
+            getAllCollections().then();
+        }
+    }, [db, isConnecting]);
 
     const handleSubmit = async (values: CreateWordDto) => {
         // Persist (copy) img from values img path;
@@ -65,7 +80,7 @@ export const WordFormScreen = ({ navigation }: WordFormProps) => {
                     <Text style={styles.title}>Створити нову картку</Text>
                 </View>
                 <View>
-                    <WordForm onSubmit={handleSubmit} collections={[]} />
+                    <WordForm onSubmit={handleSubmit} collections={collections} />
                 </View>
             </SafeAreaView>
         </>
